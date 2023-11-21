1fef6f0b220e02e144ddad0eaea60f5bca99801a
antigonewong <antigonew2024@student.cis.edu.hk>
2023-11-21 14:59:51 +0800

    final lab

--- src/CISQueue.java
+++ src/CISQueue.java
@@ -1,6 +1,9 @@
 import java.util.LinkedList;
+import java.util.Queue;
 
 public class CISQueue {
+
+    private Queue<Integer> queue = new LinkedList<>();
 
     // Linked list property.
 
@@ -8,15 +11,45 @@ public class CISQueue {
 
     // Constructor.
 
+    public CISQueue(Queue<Integer> queue) {
+        this.queue = queue;
+    }
+
+    public CISQueue() {
+    }
+
     // Enqueue. This method adds a node to the end of the linked list.
+
+    public void enqueue(int e) {
+        queue.add(e);
+    }
 
     // Dequeue. This method removes a node from the beginning of the linked list.
 
+    public Integer dequeue() {
+        return queue.poll();
+    }
+
     // isEmpty. Returns a boolean indicating whether the linked list is empty.
+
+    public boolean isEmpty() {
+        return queue.isEmpty();
+    }
 
     // size. Returns the size of the queue.
 
+    public int size() {
+        return queue.size();
+    }
+
     // toString. Returns a description of the queue in, for example, the following format:
     // CISQueue{queue=[7, 11], size=2}
+
+    public String toString() {
+        String s = "";
+        s = s + "CISQueue{queue=" + queue;
+        s = s + "], size=" + queue.size() + "}";
+        return s;
+    }
 
 }
